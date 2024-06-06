f0a42d28a7204a2b3da4047377dc5ebf692fd649
NikoQT <146917388+NikoQT@users.noreply.github.com>
2024-06-06 12:24:42 +0200

    added features "change mode" and "reboot task"

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # Shutdown Manager
- A simple program which allows you to manage batch based shutdown commands for Windows
+ A simple program which allows you to manage batch based shutdown commands for Microsoft Windows
 
  Installation:
- Currently the program is portable so all you need to do is download the .exe file from the release tab and download it.
+ Currently the program is portable so all you need to do is download the .exe file from the release tab and run it.
 
 Planned Features:
-- create new shutdown task: ✅
-- delete all shutdown tasks: ✅
+- create same day shutdown task: ✅
+- delete same day shutdown tasks: ✅
+- reboot task: ✅
 - reading remaining time until shutdown:
-- managing multiple also far future shutdowns:
+- creating (far) future shutdown:
+- delete (far) future shutdown:
+- managing multiple (far) future shutdowns:
+- change mode (change between different features): ✅
 - gui:
--- ShutdownManager.cpp
+++ ShutdownManager.cpp
@@ -4,20 +4,72 @@
 
 using namespace std;
 
+void build(int unitm, string duration, string mode);
+void task(string& mode);
+void unit(int& unitm);
+void time(string& duration);
+
 int main()
 {
-    string timeInSeconds;
-    cout << "Enter desired time in seconds. Enter \"stop\" to cancel all running shutdown timers: ";
-    cin >> timeInSeconds;
+    string duration, mode;
+    int input, unitm;
+    cout << "main mode:\n";
+    cout << "[1] create new task\n";
+    cout << "[2] cancel existing task\n";
+    cin >> input;
 
-    if (timeInSeconds == "stop") {
+    if (input == 2) {
        string s = "shutdown -a";
        system(s.c_str());
        return 0;
     }
 
-    string s = "shutdown -s -t " + timeInSeconds;
-    system(s.c_str());
+    task(mode);
+    unit(unitm);
+    time(duration);
+    build(unitm, duration, mode);
     return 0;
 }
 
+void task(string& mode) {
+    system("cls");
+    int input; 
+    cout << "task mode:\n";
+    cout << "[1] new shutdown timer\n";
+    cout << "[2] new reboot timer\n";
+    cin >> input;
+
+    switch (input) {
+        case 1: mode = " -s"; break;
+        case 2: mode = " -r"; break;
+    }
+}
+
+void unit(int& unitm) {
+    system("cls");
+    int input;
+    cout << "unit mode:\n";
+    cout << "[1] seconds\n";
+    cout << "[2] minutes\n";
+    cout << "[3] hours\n";
+    cin >> input;
+
+    switch (input) {
+    case 1: unitm = 1; break;
+    case 2: unitm = 60; break;
+    case 3: unitm = 3600; break;
+    }
+}
+
+void time(string& duration) {
+    system("cls");
+    cout << "duration: ";
+    cin >> duration;
+}
+
+void build(int unitm, string duration, string mode) {
+
+    duration = " -t " + to_string(stoi(duration) * unitm);
+    string command = "shutdown" + mode + duration;
+    system(command.c_str());
+}
