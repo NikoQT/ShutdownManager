bfa338b4da4c42c6fe64dd98ae944c9615a1a5a1
NikoQT <146917388+NikoQT@users.noreply.github.com>
2024-06-17 19:31:52 +0200

    WOP: Xml builder

--- ShutdownManager.cpp
+++ ShutdownManager.cpp
@@ -2,20 +2,25 @@
 #include <iostream>
 #include <string>
 #include <time.h>
+#include <fstream>
+#include <filesystem>
 #include "coutColors.h"
 using namespace std;
 
 void buildTask(string duration, int unitm, string mode);
 void createTask();
 void cancelSameDayTask();
+void buildXML();
 int inputUnit();
 int calcTime(int hourInput, int minInput, int secInput);
 string inputRelTime();
 string inputSpecificTime();
 string getUserSID();
+string getCurrentUser();
 
 int main()
 { 
+    buildXML();
     int input;
     cout << "main mode:\n";
     cout << "[1] create new task\n";
@@ -158,4 +163,99 @@ string getUserSID() {
     else {
         throw runtime_error("SID not found in whoami output");
     }
+}
+
+string getCurrentUser() {
+
+    char buffer[128];
+    string result;
+    FILE* pipe = _popen("whoami", "r");
+    if (!pipe) throw runtime_error("popen() failed!");
+
+    while (fgets(buffer, sizeof(buffer), pipe) != nullptr) {
+        result += buffer;
+    }
+
+    _pclose(pipe);
+
+    size_t pos = result.find('\n');
+    if (pos != string::npos) {
+        result.erase(pos);
+    }
+
+    return result;
+}
+
+void buildXML() {
+    string date = "2024-06-17T12:11:54.6558387";
+    string author = getCurrentUser();
+    string startBoundary = "2024-06-17T13:10:43+02:00";
+    string endBoundary = "2025-06-17T14:10:43+02:00";
+    string userId = getUserSID();
+    string command = "shutdown";
+    string arguments = "-s -t 60";
+
+    ofstream file("D:\\Downloads\\task.xml");
+
+    if (!file) {
+        cerr << "Error creating file!" << endl;
+        system("pause");
+    }
+
+    file << R"(<?xml version="1.0" encoding="UTF-8"?>)" << endl;
+    file << R"(<Task xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task" version="1.4">)" << endl;
+    file << R"(  <RegistrationInfo>)" << endl;
+    file << "    <Date>" << date << "</Date>" << endl;
+    file << "    <Author>" << author << "</Author>" << endl;
+    file << R"(    <URI>\Atest</URI>)" << endl;
+    file << R"(  </RegistrationInfo>)" << endl;
+    file << R"(  <Triggers>)" << endl;
+    file << R"(    <TimeTrigger>)" << endl;
+    file << "      <StartBoundary>" << startBoundary << "</StartBoundary>" << endl;
+    file << "      <EndBoundary>" << endBoundary << "</EndBoundary>" << endl;
+    file << R"(      <Enabled>true</Enabled>)" << endl;
+    file << R"(    </TimeTrigger>)" << endl;
+    file << R"(  </Triggers>)" << endl;
+    file << R"(  <Principals>)" << endl;
+    file << R"(    <Principal id="Author">)" << endl;
+    file << "      <UserId>" << userId << "</UserId>" << endl;
+    file << R"(      <LogonType>S4U</LogonType>)" << endl;
+    file << R"(      <RunLevel>LeastPrivilege</RunLevel>)" << endl;
+    file << R"(    </Principal>)" << endl;
+    file << R"(  </Principals>)" << endl;
+    file << R"(  <Settings>)" << endl;
+    file << R"(    <MultipleInstancesPolicy>IgnoreNew</MultipleInstancesPolicy>)" << endl;
+    file << R"(    <DisallowStartIfOnBatteries>false</DisallowStartIfOnBatteries>)" << endl;
+    file << R"(    <StopIfGoingOnBatteries>true</StopIfGoingOnBatteries>)" << endl;
+    file << R"(    <AllowHardTerminate>true</AllowHardTerminate>)" << endl;
+    file << R"(    <StartWhenAvailable>true</StartWhenAvailable>)" << endl;
+    file << R"(    <RunOnlyIfNetworkAvailable>false</RunOnlyIfNetworkAvailable>)" << endl;
+    file << R"(    <IdleSettings>)" << endl;
+    file << R"(      <StopOnIdleEnd>true</StopOnIdleEnd>)" << endl;
+    file << R"(      <RestartOnIdle>false</RestartOnIdle>)" << endl;
+    file << R"(    </IdleSettings>)" << endl;
+    file << R"(    <AllowStartOnDemand>true</AllowStartOnDemand>)" << endl;
+    file << R"(    <Enabled>true</Enabled>)" << endl;
+    file << R"(    <Hidden>false</Hidden>)" << endl;
+    file << R"(    <RunOnlyIfIdle>false</RunOnlyIfIdle>)" << endl;
+    file << R"(    <DisallowStartOnRemoteAppSession>false</DisallowStartOnRemoteAppSession>)" << endl;
+    file << R"(    <UseUnifiedSchedulingEngine>true</UseUnifiedSchedulingEngine>)" << endl;
+    file << R"(    <WakeToRun>false</WakeToRun>)" << endl;
+    file << R"(    <ExecutionTimeLimit>PT72H</ExecutionTimeLimit>)" << endl;
+    file << R"(    <Priority>7</Priority>)" << endl;
+    file << R"(    <RestartOnFailure>)" << endl;
+    file << R"(      <Interval>PT1M</Interval>)" << endl;
+    file << R"(      <Count>3</Count>)" << endl;
+    file << R"(    </RestartOnFailure>)" << endl;
+    file << R"(  </Settings>)" << endl;
+    file << R"(  <Actions Context="Author">)" << endl;
+    file << R"(    <Exec>)" << endl;
+    file << "      <Command>" << command << "</Command>" << endl;
+    file << "      <Arguments>" << arguments << "</Arguments>" << endl;
+    file << R"(    </Exec>)" << endl;
+    file << R"(  </Actions>)" << endl;
+    file << R"(</Task>)" << endl;
+
+    file.close();
+    cout << "XML file created successfully!" << endl;
 }
